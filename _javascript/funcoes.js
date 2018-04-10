a75b011b53654b929e950177d19188fa38cab8f5
Mucida-1 <davimucidaace@gmail.com>
2018-04-10 20:33:21 -0300

    primeira aula de java no site

---
+++ _javascript/funcoes.js
@@ -0,0 +1,27 @@
+		
+	function troca_imagem(li){
+		if (li=="especificacoes") {
+			document.getElementById('icone').src="_imagens/especificacoes.png";
+		}
+		
+		if (li=="fotos") {
+			document.getElementById('icone').src="_imagens/fotos.png";
+		}
+		
+		if (li=="multimidia") {
+			document.getElementById('icone').src="_imagens/multimidia.png";
+		}
+		
+		if (li=="contato") {
+			document.getElementById('icone').src="_imagens/contato.png";
+		}
+	};
+	function volta_imagem(){
+		document.getElementById('icone').src="_imagens/glass-oculos-preto-peq.png";
+	};
+	
+
+
+	
+	 
+
